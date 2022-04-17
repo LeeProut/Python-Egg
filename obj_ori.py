0a202a443c4682ac0bf38a7bdff049092beac3ff
Lee Prout <wleeprout@gmail.com>
2022-04-17 19:25:03 -0400

    practice on objects

---
+++ obj_ori.py
@@ -0,0 +1,16 @@
+# VS code for CWM and ch 10 practice 
+
+class Cat: 
+    pass
+
+a_cat = Cat() 
+print(a_cat)  
+
+# assign attritbutes to object
+a_cat.age = 13
+a_cat.name = "Bagheera"
+a_cat.nemesis = another_cat
+
+another_cat.age = 9
+another_cat.name = "Chicken Wang"
+
